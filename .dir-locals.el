581ac976a7b971b49ba1ae4a7d8706e7b3dc97b8
Ethan Moss <cywinskimoss@gmail.com>
2025-09-22 11:31:10 +0100

    First Initial Test

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((auto-mode-alist . (("\\.\\(defconfig\\|sheild\\)$" . conf-mode)
+                     ("\\.\\(keymap\\|overlay\\|dtsi\\)$" . c-mode))))
